e12698a04c00cd69318bedd995f557624e8d4099
Tõnis Põld <89923736+tonispold@users.noreply.github.com>
2022-06-14 09:23:18 +0300

    Add files via upload
    LMAO KEKW

---
+++ upload.js
@@ -0,0 +1,40 @@
+// Currently uploaded files
+let uploadedFiles = [];
+
+const appendFilesToList = () => {
+    const filesContainer = document.getElementById('files-container');
+    // Clear files from DOM
+    filesContainer.innerHTML = '';
+
+    // Loop through files and append to files-container
+    Array.from(uploadedFiles).forEach(file => {
+        const li = document.createElement("li");
+        const fileInfoTextNode = document.createTextNode(`${file?.name} ${file?.path}`);
+        li.appendChild(fileInfoTextNode);
+        filesContainer.appendChild(li);
+    });
+}
+
+const clearFiles = () => {
+    uploadedFiles = [];
+    const filesContainer = document.getElementById('files-container');
+    filesContainer.innerHTML = '';
+    console.log(uploadedFiles);
+}
+
+const onFileUpload = (event) => {
+    uploadedFiles = event?.target?.files;
+    console.log(event?.target?.files);
+    appendFilesToList()
+}
+
+const fileInput = document.getElementById('fileid');
+document.getElementById('buttonid').addEventListener('click', openDialog);
+function openDialog() {
+    fileInput.click();
+}
+
+fileInput.addEventListener('change', onFileUpload);
+
+const clearButton = document.getElementById('clearfiles');
+clearButton.addEventListener('click', clearFiles);
